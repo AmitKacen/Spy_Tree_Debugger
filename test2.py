9e05e599b021b4fb0cb7bdeec6c7343ffe1a1577
Amit Kacen <163858829+AmitKacen@users.noreply.github.com>
2025-12-15 17:22:46 +0200

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-from test_avl import AVLTree
-if __name__ == "__main__":
-    tree = AVLTree()
-    values = [6, 7, 8, 5, 10]
-    for value in values:
-        tree.insert(value, "a")
-    tree.delete(7)
-    print("fdfdfdf")
